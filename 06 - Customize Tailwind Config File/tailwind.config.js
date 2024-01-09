bea2db895a365967cd25cfc60ab599df7b0ca124
sohail019 <shaikhhsohail0193@gmail.com>
2024-01-10 00:11:39 +0530

    Done

--- 06 - Customize Tailwind Config File/tailwind.config.js
+++ 06 - Customize Tailwind Config File/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "10xl": ["10rem", { lineHeight: "1.2" }],
+      },
+    },
   },
   plugins: [],
 };
